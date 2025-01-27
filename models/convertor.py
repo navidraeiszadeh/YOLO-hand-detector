f2f3f20c86d431ce32399ebaa87d1ab4edc92167
Navid Raeiszadeh <Navidraeiszadeh@gmail.com>
2025-01-27 08:15:04 +0330

    feat: add a convertor to convert weight model to pt

---
+++ models/convertor.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("D:\yolo-hand-detection-master\yolo-hand-detection-master\models\convertor_YOLO")
+from yolov5.models.yolo import Model
+
+from yolov5.utils.torch_utils import load_darknet_weights
+
+model = Model("models/cross-hands-tiny.cfg")
+load_darknet_weights(model, "models/cross-hands-tiny.weights")
+model.save("cross-hands-tiny.pt")
+print("YOLO Darknet model converted to PyTorch format successfully")
